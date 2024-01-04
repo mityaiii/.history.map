2dd8a9452a0926446b24221872a2f37c30a3b7fc
Mike de Geofroy <59496187+mikedegeofroy@users.noreply.github.com>
2024-01-04 13:31:26 +0300

    Added filter to map

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,53 +1,59 @@
 'use client';
 
 import Map from '@/app/components/map/Map';
+import { FilterSection } from '../notes/components/filter-section/filter-section';
 
 export default function MapPage() {
   return (
-    <Map
-      data={{
-        type: 'FeatureCollection',
-        features: [
-          {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [30.3083, 59.9572],
+    <>
+      <div className='absolute top-20 z-10'>
+        <FilterSection />
+      </div>
+      <Map
+        data={{
+          type: 'FeatureCollection',
+          features: [
+            {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [30.3083, 59.9572],
+              },
+              properties: {
+                note_id: 1,
+                name: 'Itmo University2',
+              },
             },
-            properties: {
-              note_id: 1,
-              name: 'Itmo University2',
+            {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [30.3093, 59.9582],
+              },
+              properties: {
+                note_id: 1,
+                name: 'Itmo University3',
+              },
             },
-          },
-          {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [30.3093, 59.9582],
+            {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [30.3063, 59.9562],
+              },
+              properties: {
+                note_id: 1,
+                name: 'Itmo University',
+              },
             },
-            properties: {
-              note_id: 1,
-              name: 'Itmo University3',
-            },
-          },
-          {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [30.3063, 59.9562],
-            },
-            properties: {
-              note_id: 1,
-              name: 'Itmo University',
-            },
-          },
-        ],
-      }}
-      initalState={{
-        lng: 30.3609,
-        lat: 59.9311,
-        zoom: 12,
-      }}
-    />
+          ],
+        }}
+        initalState={{
+          lng: 30.3609,
+          lat: 59.9311,
+          zoom: 12,
+        }}
+      />
+    </>
   );
 }
